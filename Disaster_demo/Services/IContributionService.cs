e8f42b8c1d59cf74b5f8a522a0843a68896d6c3d
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-22 14:49:23 +0530

    Update IContributionService.cs

--- Disaster_demo/Services/IContributionService.cs
+++ Disaster_demo/Services/IContributionService.cs
@@ -1,4 +1,4 @@
-﻿// Services/ContributionService.cs
+// Services/ContributionService.cs
 using Disaster_demo.Models.Entities;
 
 namespace Disaster_demo.Services
@@ -6,8 +6,11 @@ namespace Disaster_demo.Services
     public interface IContributionService
     {
         Task<bool> AddContributionAsync(ContributionDTO dto);
+        Task<bool> ApproveContributionAsync(int contributionId);
         Task<List<Contribution>> GetContributionsByVolunteerIdAsync(int volunteerId);
         Task<Contribution> GetLatestContributionAsync(int volunteerId);
+        Task<List<VolunteerContributionDTO>> GetPendingContributionsAsync(string division);
         Task<int> GetTotalContributionsCountAsync(int volunteerId);
+        Task<bool> RejectContributionAsync(int contributionId);
     }
 }
